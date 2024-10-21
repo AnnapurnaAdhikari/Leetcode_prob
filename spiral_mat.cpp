50842329a9c05edc145783b8c762f2d38a94d7b9
Anna <adhikariannapurna2201@gmail.com>
2024-10-21 20:02:36 +0530

    1. Two Sum -> using bruteforce

--- spiral_mat.cpp
+++ spiral_mat.cpp
@@ -45,27 +45,46 @@ vector<int> spiralOrder02(vector<vector<int>>& matrix)
     int curr_r = 0, curr_c = 0, curr_r_len = len, curr_c_len = matrix[0].size();
     int r = 1, d = 0, u = 0, l = 0;
     vector<int> sp;
+    if(len == 1 || matrix[0].size() == 1)
+    {
+        for(int i = 0; i < len; i++)
+        {
+            for(int j = 0; j < matrix[0].size(); j++)
+            {
+                sp.push_back(matrix[i][j]);
+            }
+        }
+        return sp;
+    }
+    else{
     while(curr_c_len != 0 && curr_r_len != 0){
     if (r == 1){
         for(; curr_c < curr_c_len; curr_c++)
         {
             cout<<" r = 1 -> curr_r: "<<curr_r<<" "<<"curr_c: "<<curr_c<<"\n";
-            cout<<"curr_r_len: "<<curr_r_len<<"\n";
+            // cout<<"curr_r_len: "<<curr_r_len<<"\n";
             sp.push_back(matrix[curr_r][curr_c]);
         }
+        //if(curr_c_len) 
         curr_c_len--;
         curr_r ++; curr_c--;
         r = 0; d = 1;
+        cout<<"curr_c_len: "<<curr_c_len<<"\n";
+        cout<<"curr_r_len: "<<curr_r_len<<"\n";
         }
     else if(d == 1){
         for(; curr_r < curr_r_len; curr_r++)
         {
             cout<<" d = 1 -> curr_r: "<<curr_r<<" "<<"curr_c: "<<curr_c<<"\n";
-            cout<<"curr_r_len: "<<curr_r_len<<"\n";
+            // cout<<"curr_r_len: "<<curr_r_len<<"\n";
             sp.push_back(matrix[curr_r][curr_c]);
         }
-        curr_r_len--; curr_r--; curr_c--;
+        //if (curr_r_len > 1) 
+        curr_r_len--; 
+        curr_r--; curr_c--;
         d = 0; l = 1;
+        cout<<"curr_r_len: "<<curr_r_len<<"\n";
+        cout<<"curr_c_len: "<<curr_c_len<<"\n";
     }
     else if (l == 1)
     {
@@ -75,8 +94,11 @@ vector<int> spiralOrder02(vector<vector<int>>& matrix)
             sp.push_back(matrix[curr_r][curr_c]);
         }
         curr_r--;curr_c++;
+        //if(curr_c_len > 1) 
         curr_c_len--;
         l = 0; u = 1;
+        cout<<"curr_r_len: "<<curr_r_len<<"\n";
+        cout<<"curr_c_len: "<<curr_c_len<<"\n";
     }
     else if(u == 1)
     {
@@ -85,25 +107,37 @@ vector<int> spiralOrder02(vector<vector<int>>& matrix)
             cout<<" u = 1 -> curr_r: "<<curr_r<<" "<<"curr_c: "<<curr_c<<"\n";
             sp.push_back(matrix[curr_r][curr_c]);
         }
-        curr_r++; curr_r_len--; curr_c++; curr_c_len++;
+        curr_r++;  curr_c++; curr_c_len++;
+        //if(curr_r_len > 1) 
+        curr_r_len--;
         u = 0; r = 1;
     }
     }
     return sp;
+    }
 }
 int main()
 {
     vector<vector<int>> m1 = {{1,2,3}, {4,5,6}, {7,8,9}};
     vector<vector<int>> m2 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
-    vector<vector<int>> m3 = {{3}, {2}};
+    vector<vector<int>> m6 = {{1,2,3}, {4,5,6}, {7,8,9},{10,11,12}};
+    vector<vector<int>> m5 = {{2,4}, {8,5}, {0,-1}};
+    // vector<vector<int>> m3 = {{3}, {2}};
+    // vector<vector<int>> m4 = {{3,2}};
     // vector<int> ma1 = spiralOrder01(m1);
     // vector<int> ma02 = spiralOrder02(m1);
     // vector<int> ma2 = spiralOrder02(m2);
-    vector<int> ma3 = spiralOrder02(m3);
+    // vector<int> ma3 = spiralOrder02(m3);
+    // vector<int> ma4 = spiralOrder02(m4);
+    vector<int> ma5 = spiralOrder02(m5);
+    vector<int> ma6 = spiralOrder02(m6);
     // for (int x: ma02) cout<<x<<" ";
     // cout<<"\n";
     // for (int x: ma2) cout<<x<<" ";
     // cout<<"\n";
-    for (int x: ma3) cout<<x<<" ";
+    // for (int x: ma3) cout<<x<<" ";
+    // for (int x: ma4) cout<<x<<" ";
+    // for (int x: ma5) cout<<x<<" ";
+    for (int x: ma6) cout<<x<<" ";
     return 0;
 }
---
+++ two_sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> indices;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    // indices.push_back(i);
+                    // indices.push_back(j);
+                    indices = {i, j};
+                }
+            }
+            if(indices.size() == 2) return indices;
+        }
+    }
+
+int main()
+{
+    vector<int> n = {2,7,11,15};
+    vector<int> n2 = {3,2,4};
+    int t = 9;
+    int t2 = 6;
+    vector<int> ans = twoSum(n, t);
+    vector<int> ans2 = twoSum(n2, t2);
+    for(int x: ans) cout<<x<<" ";
+    for(int x: ans2) cout<<x<<" ";
+    return 0;
+}
